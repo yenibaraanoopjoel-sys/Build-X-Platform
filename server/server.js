f25364826086eb94eeb2bb78edd4bdb8beaecbd9
Anoop Joel <yenibara.anoopjoel@gmail.com>
2026-05-18 15:36:43 +0530

    Fixed Render environment loading

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
+
 const cors = require("cors");
-const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
 
 const errorHandler = require("./middleware/errorMiddleware");
-
-// Load environment variables
-dotenv.config();
 
 // Connect Database
 connectDB();
@@ -17,7 +16,25 @@ const app = express();
 
 // Middleware
 app.use(cors());
+
 app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// Debug Environment Variables
+console.log(
+  "OPENROUTER KEY EXISTS:",
+  !!process.env.OPENROUTER_API_KEY
+);
+
+console.log(
+  "MONGO URI EXISTS:",
+  !!process.env.MONGO_URI
+);
 
 // Routes
 app.use(
@@ -62,20 +79,23 @@ app.use(
   require("./routes/aiRoutes")
 );
 
-// Test Route
+// Root Route
 app.get("/", (req, res) => {
-  res.send("BuildX API Running 🚀");
+  res.send(
+    "BuildX API Running 🚀"
+  );
 });
 
 // Error Middleware
 app.use(errorHandler);
 
-// Start Server
+// PORT
 const PORT =
   process.env.PORT || 5000;
 
+// Start Server
 app.listen(PORT, () => {
   console.log(
-    `Server running on port ${PORT}`
+    `🚀 Server running on port ${PORT}`
   );
 });
