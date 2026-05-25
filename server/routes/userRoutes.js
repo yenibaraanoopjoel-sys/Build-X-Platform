58afaf959299ee4ae3c99cbb17f0825362e1949b
Anoop Joel <yenibara.anoopjoel@gmail.com>
2026-05-25 14:28:21 +0530

    Fixed all-users route crash

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,12 +6,68 @@ const protect = require("../middleware/authMiddleware");
 
 const User = require("../models/User");
 
-router.get("/profile", protect, async (req, res) => {
-  const user = await User.findById(req.user).select(
-    "-password"
-  );
+//
+// GET PROFILE
+//
+router.get(
+  "/profile",
+  protect,
+  async (req, res) => {
+    try {
+      const user =
+        await User.findById(
+          req.user
+        ).select(
+          "-password"
+        );
 
-  res.json(user);
-});
+      res.json({
+        success: true,
+
+        user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+
+        message:
+          error.message,
+      });
+    }
+  }
+);
+
+//
+// GET ALL USERS
+//
+router.get(
+  "/all-users",
+  protect,
+  async (req, res) => {
+    try {
+      const users =
+        await User.find()
+          .select(
+            "-password"
+          )
+          .sort({
+            createdAt: -1,
+          });
+
+      res.json({
+        success: true,
+
+        users,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+
+        message:
+          error.message,
+      });
+    }
+  }
+);
 
 module.exports = router;
