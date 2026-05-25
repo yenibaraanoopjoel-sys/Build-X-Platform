acd2a4bb1c008f6cf468ddadc935f526366e2cfd
Anoop Joel <yenibara.anoopjoel@gmail.com>
2026-05-25 14:01:23 +0530

    Fixed collaboration requests notifications and request center

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -10,4 +10,28 @@ const API = axios.create({
   },
 });
 
+//
+// AUTO ATTACH TOKEN
+//
+API.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem(
+        "token"
+      );
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(
+      error
+    );
+  }
+);
+
 export default API;
