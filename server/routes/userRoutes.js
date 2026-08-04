a2e69d06819bb1e155c41cb7ad487e175273ed3e
Anoop Joel <yenibara.anoopjoel@gmail.com>
2026-08-04 16:51:34 +0530

    Add getAllUsers API

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,12 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
+//
+// MIDDLEWARE
+//
 const protect = require("../middleware/authMiddleware");
 
+//
+// CONTROLLERS
+//
 const {
   getUserProfile,
   updateUserProfile,
+  getAllUsers,
 } = require("../controllers/userControllers");
+
+//
+// GET ALL USERS
+//
+router.get(
+  "/all",
+  protect,
+  getAllUsers
+);
 
 //
 // GET PROFILE
